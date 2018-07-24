330e7e3a6811c84ee59530df56c25c85be7efb6b
Ankur Vineet <vineetankuro2@gmail.com>
2018-07-25 00:37:28 +0530

    Screenshot Added

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(numbersIntent);
             }
         });
+
         TextView colorsTextview = findViewById(R.id.colors);
         colorsTextview.setOnClickListener(new View.OnClickListener() {
             @Override
